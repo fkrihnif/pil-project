4832a62ffd6ac0062c589740e33d5ea6336cef74
Fikri Hanif <fikrihanif77@gmail.com>
2024-01-22 11:21:57 +0700

    first commit

---
+++ database/seeders/SuperAdminSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class SuperAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Creating Super Admin User
+        $superAdmin = User::create([
+            'name' => 'SuperAdmin', 
+            'username' => 'superadmin', 
+            'email' => 'superadmin@pil.com',
+            'password' => Hash::make('password123')
+        ]);
+        $superAdmin->assignRole('Super Admin');
+
+        // Creating Admin User
+        $admin = User::create([
+            'name' => 'Admin', 
+            'username' => 'admin', 
+            'email' => 'admin@pil.com',
+            'password' => Hash::make('password')
+        ]);
+        $admin->assignRole('Admin');
+    }
+}
